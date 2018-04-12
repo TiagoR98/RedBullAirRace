4b8e3b0cd8ca1c81d0840bef89fbe3f28cdfba33
TiagoR98 <tiago7b27@gmail.com>
2018-04-12 17:04:36 +0100

    init commit

---
+++ android/src/com/redbull/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.redbull.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.redbull.game.RedBullGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RedBullGame(), config);
+	}
+}
