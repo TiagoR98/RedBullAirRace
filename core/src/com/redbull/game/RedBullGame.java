c87c29e46078281c56b365e08cd161c59d70ddf3
Tiago Ribeiro <tiago7b27@gmail.com>
2018-04-12 23:37:09 +0100

    init2

--- core/src/com/redbull/game/RedBullGame.java
+++ core/src/com/redbull/game/RedBullGame.java
@@ -5,28 +5,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class RedBullGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-        BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("quim.jpg");
-                font  = new BitmapFont(Gdx.files.internal("Vanilla Galaxies by Breely.ttf"));
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 200, 0);
-                //font.getData().setScale(2);
-                font.draw(batch, "Quim das Moças Bem alimentado", 0, 300);
+		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
