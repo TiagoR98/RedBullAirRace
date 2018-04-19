6c729b2bd43dcad48f9f01fb2b33bb670242c47f
Bruno Oliveira <bedgar1@gmail.com>
2018-04-19 19:05:19 +0100

    1st background

--- core/src/com/redbull/game/RedBullGame.java
+++ core/src/com/redbull/game/RedBullGame.java
@@ -5,24 +5,42 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class RedBullGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	Texture back;
+	Sprite backSprite;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		back = new Texture("bakcg.png");
+		backSprite = new Sprite(back);
 	}
+
+	int x = 0;
+	int y = 0;
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+
 		batch.draw(img, 0, 0);
+		backSprite.setX(x++);
+		backSprite.setY(0);
+		backSprite.setScale(2,2);
+		backSprite.draw(batch);
 		batch.end();
+
+
+
+
 	}
 	
 	@Override
