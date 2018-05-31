5b9ce175843ed5b381ea6e98188bfa67c697e4be
Bruno Oliveira <bedgar1@gmail.com>
2018-06-01 00:00:01 +0100

    aviao challenger + menu back + logo

--- core/src/com/redbull/game/view/MainMenu.java
+++ core/src/com/redbull/game/view/MainMenu.java
@@ -3,10 +3,12 @@ package com.redbull.game.view;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -34,6 +36,18 @@ public class MainMenu extends ScreenAdapter {
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
         mySkin = new Skin(Gdx.files.internal("skin/clean-crispy-ui.json"));
+
+
+        Texture texture = new Texture(Gdx.files.internal("backgmenu.png"));
+        Image back = new Image(texture);
+        back.setPosition(0,0);
+        stage.addActor(back);
+
+        Texture texture2 = new Texture(Gdx.files.internal("rbarlogowht.png"));
+        Image rbarlogo = new Image(texture2);
+        back.setPosition(0,0);
+        stage.addActor(rbarlogo);
+
 
         Button button2 = new TextButton("Start Game",mySkin);
         button2.setSize(col_width*8,row_height*2);
@@ -66,12 +80,11 @@ public class MainMenu extends ScreenAdapter {
         return game;
     }
 
+
     @Override
     public void render (float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-
 
 
         stage.act();
